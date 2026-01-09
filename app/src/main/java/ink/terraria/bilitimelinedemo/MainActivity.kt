83b6e20a4416fd04f0702a933ff5484eb84865f7
Cai <13110818005@qq.com>
2026-01-09 21:19:26 +0800

    feat: add bottom spacer to fix clipped card display

--- app/src/main/java/ink/terraria/bilitimelinedemo/MainActivity.kt
+++ app/src/main/java/ink/terraria/bilitimelinedemo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -35,6 +36,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -117,6 +119,10 @@ class MainActivity : ComponentActivity() {
                     items(displayUPs) { post ->
                         PostCard(post)
                     }
+
+                    item {
+                        Spacer(modifier = Modifier.padding(bottom = 16.dp))
+                    }
                 }
             }
 
@@ -142,14 +148,11 @@ class MainActivity : ComponentActivity() {
 
 
             LazyRow(
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 16.dp)
             ) {
                 items(ups) { data ->
-
-
                     UpAvatar(data)
                 }
             }
