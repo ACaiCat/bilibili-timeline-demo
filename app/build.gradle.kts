16084660b97a6c24c515eddb7555cee3bd180aff
Cai <13110818005@qq.com>
2026-01-09 21:20:21 +0800

    chore: bump version to v3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "ink.terraria.bilitimelinedemo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
